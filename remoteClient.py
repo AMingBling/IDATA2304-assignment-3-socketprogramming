9f11ae380ddd3f5c0e73badfcccd96b870db2050
AMingBling <annelimb@stud.ntnu.no>
2025-10-21 21:22:09 +0200

    Updated tui

--- remoteClient.py
+++ remoteClient.py
@@ -1,13 +1,19 @@
 import socket
 
+def display_status(sock):
+    sock.sendall("6".encode())
+    status = sock.recv(1024).decode().strip()
+    print(f"\n---------- TV Status ----------\n{status}\n-------------------------------\n")
+
 def main():
     host = "127.0.0.1"
-    port = 65432
+    port = 1238
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     try:
         sock.connect((host, port))
         print(sock.recv(1024).decode())
         while True:
+            display_status(sock)
             command = input("Remote>> ")
             sock.sendall(command.encode())
             response = sock.recv(1024).decode().strip()
